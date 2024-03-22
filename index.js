793595e19f4ea4ecd9faeefaf5ea84090345d47e
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-22 07:51:30 +0530

    updated   : index

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 // index.js
 
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import userRoute from './routes/user.route.js';
-import errorHandler from './middleware/error.js'; 
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import userRoute from "./routes/user.route.js";
+import authRoute from "./routes/auth.route.js";
 
- 
+import errorHandler from "./middleware/error.js";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.DB_URL)
   .then(() => {
-    console.log('Connected to the database');
+    console.log("Connected to the database");
   })
   .catch((err) => {
-    console.error('Error connecting to the database:', err);
+    console.error("Error connecting to the database:", err);
   });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 // Enable CORS for a specific origin
-app.use(cors({ origin: '*' }));
+app.use(cors({ origin: "*" }));
 
 app.listen(process.env.PORT, () => {
-  console.log('Server listening on port ' + process.env.PORT);
+  console.log("Server listening on port " + process.env.PORT);
 });
 //basic home route
 app.get("/", (req, res) => {
   res.send("home");
 });
- 
-app.use('/user', userRoute); 
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
 
- 
- 
 app.use(errorHandler);
