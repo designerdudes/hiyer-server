9afc3e8dc2aed2f041daf8f6ae5c0a29c57c00ea
Mohd Mujahed <mujahedu984@gmail.com>
2024-03-22 18:04:57 +0530

    updated   : user.model

--- models/user.model.js
+++ models/user.model.js
@@ -1,27 +1,5 @@
 import mongoose from "mongoose";
 
-const addressSchema = new mongoose.Schema({
-  pincode: {
-    type: String,
-    required: true,
-  },
-  state: {
-    type: String,
-    required: true,
-  },
-  city: {
-    type: String,
-    required: true,
-  },
-  country: {
-    type: String,
-    required: true,
-  },
-  landmark: String,
-});
-
-mongoose.model("Address", addressSchema);
-
 const userSchema = new mongoose.Schema(
   {
     email: {
