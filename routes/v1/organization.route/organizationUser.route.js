2780b67f5e67c005dbcff7d2fd0444edacc12707
Mohd Mujahed <mujahedu984@gmail.com>
2024-06-15 12:57:38 +0530

    updated: organization

---
+++ routes/v1/organization.route/organizationUser.route.js
@@ -0,0 +1,84 @@
+import express from 'express';
+import {
+  addOrUpdateUserData,
+  addOrUpdateAddress, 
+  addOrUpdateContactInfo,
+  addOrUpdateWebsite,
+  addOrUpdateSocialLinks,
+  updateLogo,
+  updateBio,
+  addProject,
+  updateProject,
+  deleteProject,
+  addTeamMember,
+  updateTeamMember,
+  deleteTeamMember,
+  addSocialLink,
+  updateSocialLink,
+  deleteSocialLink,
+  updateUrlInProject,
+  addUrlToProject,
+  removeProjectURL,
+} from '../../../controllers/organizationUser.controller/organizationUser.js';
+
+const router = express.Router();
+
+// Route for adding or updating user data
+router.post('/user/data', addOrUpdateUserData);
+
+// Route for adding or updating address
+router.post('/user/address', addOrUpdateAddress);
+
+
+// Route for adding or updating contact information
+router.post('/user/contact-info', addOrUpdateContactInfo);
+
+// Route for adding or updating website
+router.post('/user/website', addOrUpdateWebsite);
+
+// Route for adding or updating social links
+router.post('/user/social-links', addOrUpdateSocialLinks);
+
+// Route for updating logo
+router.put('/user/logo', updateLogo);
+
+// Route for updating bio
+router.put('/user/bio', updateBio);
+
+// POST /api/user/project
+router.post('/user/project', addProject);
+
+// PUT /api/user/project/:id
+router.put('/user/project/:id', updateProject);
+
+// DELETE /api/user/project/:id
+router.delete('/user/project/:id', deleteProject);
+
+// POST /api/user/project/:id/url
+router.post('/user/project/:id/url', addUrlToProject);
+
+// PUT /api/user/project/:id/url/:urlId
+router.put('/user/project/:id/url/:urlId', updateUrlInProject);
+
+// DELETE /api/user/project/:id/url/:urlId
+router.delete('/user/project/:id/url/:urlId', removeProjectURL);
+
+// Route for adding a team member
+router.post('/user/team-member', addTeamMember);
+
+// Route for updating a team member
+router.put('/user/team-member/:teamMemberId', updateTeamMember);
+
+// Route for deleting a team member
+router.delete('/user/team-member/:teamMemberId', deleteTeamMember);
+
+// Route for adding a social link
+router.post('/user/social-link', addSocialLink);
+
+// Route for updating a social link
+router.put('/user/social-link/:socialLinkId', updateSocialLink);
+
+// Route for deleting a social link
+router.delete('/user/social-link/:socialLinkId', deleteSocialLink);
+
+export default router;
