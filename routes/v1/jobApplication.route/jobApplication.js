7d0ce6fc96534098d9222bf3e0a15532824f6cb9
Mohd Mujahed <mujahedu984@gmail.com>
2024-06-17 00:17:18 +0530

    updated: jobApplication

---
+++ routes/v1/jobApplication.route/jobApplication.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import {     addJobApplication,
+    editJobApplicationDetails,
+    editJobApplicationVideo,
+    editJobApplicationImage,
+    deleteJobApplication } from '../../../controllers/organization.controller/jobApplication.controller.js';
+// import { upload } from '../../../config/multer.js';
+ 
+import multer from 'multer';
+
+const upload = multer({ dest: 'uploads/' }); // Set up multer with appropriate storage settings
+
+const router = express.Router();
+
+router.post('/add', upload.fields([{ name: 'video', maxCount: 1 }, { name: 'image', maxCount: 1 }]), addJobApplication);
+
+// Route to edit job application details
+router.put('/edit/:id', editJobApplicationDetails);
+
+// Route to edit job application video
+router.put('/edit-video/:id', editJobApplicationVideo);
+
+// Route to edit job application image
+router.put('/edit-image/:id', editJobApplicationImage);
+
+// Route to delete a job application
+router.delete('/delete/:id', deleteJobApplication);
+
+export default router;
