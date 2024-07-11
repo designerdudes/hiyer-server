da65cacb6d47c8ba6c173a37130e679fd103d829
Mohd Mujahed <mujahedu984@gmail.com>
2024-07-12 04:17:10 +0530

    updated: index

--- index.js
+++ index.js
@@ -9,11 +9,13 @@ import organizationMemberRoute from "./routes/v1/organization.route/organization
 import jobAdsRoute from "./routes/v1/jobAds.route/jobAds.js"
 import mediaControllRoute from "./routes/v1/mediaControll.route/mediaControll.js";
 import dropDownControllRoute from "./routes/v1/api/dropdownRoutes.js";
-
+import './config/passport/auth.js'
 import authRoute from "./routes/v1/auth.route.js";
 import errorHandler from "./middleware/error.js";
 import { updateAllImageUrls, updateAllVideoUrls } from "./controllers/mediaControl.controller/mediaUpload.js";
 import JobAds from "./models/organization.model/jobAds.model.js";
+import passport from "passport";
+import session from "express-session";
 
 dotenv.config();
 
@@ -36,6 +38,18 @@ app.use(express.json());
 // Enable CORS for a specific origin
 app.use(cors({ origin: "*" }));
 
+
+// Set up session management
+app.use(session({
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }  // Set to true if using HTTPS
+}));
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 // Basic home route
 app.get("/", (req, res) => {
   res.send("home");
@@ -110,6 +124,224 @@ app.put('/update-job-ads-keys', async (req, res) => {
   }
 });
 
+
+// Route for initiating google authentication
+
+app.get('/auth/google', (req, res, next) => {
+  req.session.profileType = req.body.profileType;
+  next();
+}, passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', (req, res, next) => {
+  passport.authenticate('google', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/google/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/google/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/google/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+app.get('/auth/google/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+app.get('/auth/google/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+
+// Route for initiating Facebook authentication
+app.get('/auth/facebook', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  
+  next();
+}, passport.authenticate('facebook', { scope: ['email'] }));
+
+// Callback route after Facebook authentication
+app.get('/auth/facebook/callback', (req, res, next) => {
+  passport.authenticate('facebook', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/facebook/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/facebook/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/facebook/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/facebook/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/facebook/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+
+// Route for initiating GitHub authentication
+app.get('/auth/github', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  // Assuming you pass profileType in the request body
+  next();
+}, passport.authenticate('github'));
+
+// Callback route after GitHub authentication
+app.get('/auth/github/callback', (req, res, next) => {
+  passport.authenticate('github', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/github/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/github/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/github/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/github/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/github/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+// Route for initiating Twitter authentication
+app.get('/auth/twitter', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  // Assuming you pass profileType in the request body
+  next();
+}, passport.authenticate('twitter'));
+
+// Callback route after Twitter authentication
+app.get('/auth/twitter/callback', (req, res, next) => {
+  passport.authenticate('twitter', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/twitter/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/twitter/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/twitter/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/twitter/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/twitter/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+
+// Route for initiating Apple authentication
+app.get('/auth/apple', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  // Assuming you pass profileType in the request body
+  next();
+}, passport.authenticate('apple'));
+
+// Callback route after Apple authentication
+app.get('/auth/apple/callback', (req, res, next) => {
+  passport.authenticate('apple', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/apple/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/apple/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/apple/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/apple/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/apple/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+// Route for initiating LinkedIn authentication
+app.get('/auth/linkedin', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  // Assuming you pass profileType in the request body
+  next();
+}, passport.authenticate('linkedin'));
+
+// Callback route after LinkedIn authentication
+app.get('/auth/linkedin/callback', (req, res, next) => {
+  passport.authenticate('linkedin', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/linkedin/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/linkedin/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/linkedin/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/linkedin/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/linkedin/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
+
+// Route for initiating Microsoft authentication
+app.get('/auth/microsoft', (req, res, next) => {
+  req.session.profileType = req.body.profileType;  // Assuming you pass profileType in the request body
+  next();
+}, passport.authenticate('microsoft'));
+
+// Callback route after Microsoft authentication
+app.get('/auth/microsoft/callback', (req, res, next) => {
+  passport.authenticate('microsoft', (err, data) => {
+    if (err) {
+      return res.redirect('/auth/microsoft/failure');
+    }
+    if (!data) {
+      return res.redirect('/auth/microsoft/failure');
+    }
+    // Send the token to the frontend
+    res.redirect(`/auth/microsoft/success?token=${data.token}`);
+  })(req, res, next);
+});
+
+// Success route
+app.get('/auth/microsoft/success', (req, res) => {
+  const { token } = req.query;
+  res.json({ success: true, message: 'Authentication successful', token });
+});
+
+// Failure route
+app.get('/auth/microsoft/failure', (req, res) => {
+  res.json({ success: false, message: 'Authentication failed' });
+});
 
 
 app.use(errorHandler);
