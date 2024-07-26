d1d9e00f68dd51688b3c602b54c8d1b8c2fba128
Mohd Mujahed <mujahedu984@gmail.com>
2024-07-27 00:33:02 +0530

    updated: plan.model

---
+++ models/plan.model.js
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+
+const PlanSchema = new mongoose.Schema({
+  razorpayPlanId: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  period: {
+    type: String,
+    enum: ['daily', 'weekly', 'monthly', 'yearly'],
+    required: true
+  },
+  interval: {
+    type: Number,
+    required: true
+  },
+  item: {
+    id: String,
+    active: Boolean,
+    name: {
+      type: String,
+      required: true
+    },
+    description: String,
+    amount: {
+      type: Number,
+      required: true
+    },
+    currency: {
+      type: String,
+      required: true
+    },
+    unit_amount: Number,
+    unit: String,
+    tax_inclusive: Boolean,
+    hsn_code: String,
+    sac_code: String,
+    tax_rate: Number,
+    tax_id: String,
+    tax_group_id: String,
+    created_at: Number,
+    updated_at: Number
+  },
+  notes: {
+    type: Map,
+    of: String
+  },
+  created_at: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+const Plan = mongoose.model('Plan', PlanSchema);
+
+export default Plan;
