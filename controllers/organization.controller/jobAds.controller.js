6f72766cfe8b2b0092700ec1f64492b1d18cba9d
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-07-30 04:42:28 +0530

    updated code

--- controllers/organization.controller/jobAds.controller.js
+++ controllers/organization.controller/jobAds.controller.js
@@ -10,6 +10,7 @@ import OrganizationMember from "../../models/organizationUser.model/organization
 import JobAlert from "../../models/organization.model/jobAlert.model.js";
 import { sendApplicantStatusUpdateEmail, sendEmailAdNotification, sendNewJobAlertByUserEmail, sendNewJobAlertEmail } from "../../config/zohoMail.js";
 import IndividualUser from "../../models/individualUser.model/individualUser.model.js";
+import path from "path";
 
 export const notifyUsersOfNewJob = async (jobId, orgId) => {
   try {
@@ -767,13 +768,14 @@ export const getJobAdsDetailsForPoster = async (req, res) => {
       })
       .populate({
         path: 'applicants.user',
-        select: 'name email profilePicture lastLoggedIn profile',
+        select: 'name email profilePicture lastLoggedIn',
         populate: {
           path: 'profilePicture',
           model: 'Image'
         }
 
-      });
+      })
+
 
     if (!jobAds) {
       return res.status(404).json({ error: 'Job application not found' });
@@ -807,11 +809,17 @@ export const getJobAdsDetailsForPoster = async (req, res) => {
 
         populatedApplicant.resumeVideo = await mongoose.model(mediaModel).findById(populatedApplicant.resumeVideo.mediaRef).populate('thumbnailUrl');
       }
+
+
       // Check if user is populated and calculate the active field
       if (populatedApplicant.user) {
         const lastLoggedInDate = new Date(populatedApplicant.user.lastLoggedIn);
+        const userData = await IndividualUser.findById(populatedApplicant.user._id, 'skills industry')
+
+        console.log(userData)
         const isActive = (new Date() - lastLoggedInDate) < (2 * 24 * 60 * 60 * 1000); // 2 days in milliseconds
         populatedApplicant.user.active = isActive;
+        populatedApplicant.user.filterData = userData
       }
 
       return isPoster ? populatedApplicant : { user: populatedApplicant.user };
