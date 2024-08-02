203cacf7730fb3cb67cbfe8ea1e04ebe4fee16a9
Mohd Mujahed <mujahedu984@gmail.com>
2024-08-03 01:27:28 +0530

    updated: user.controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,12 +27,12 @@ export const sendEmailOTPforverification = async (req, res) => {
     const { email } = req.body;
     const validEmailUser = await User.findOne({ "email.id": email });
 
-    if (!validEmailUser) {
-      return res.status(404).json({
-        ok: false,
-        msg: "User not found",
-      });
-    }
+    // if (!validEmailUser) {
+    //   return res.status(404).json({
+    //     ok: false,
+    //     msg: "User not found",
+    //   });
+    // }
 
     let OTP = Math.floor(Math.random() * 900000) + 100000; // Generate a random 6-digit OTP
     console.log("OTP is generated", OTP);
@@ -51,7 +51,7 @@ export const sendEmailOTPforverification = async (req, res) => {
 
     res.status(200).json({
       ok: true,
-      msg: "Email sent to existing user",
+      msg: validEmailUser ? "Email sent to existing user" : "Email sent to new user",
     });
   } catch (error) {
     console.error("Error in sending OTP for verification:", error);
@@ -355,8 +355,8 @@ export const registerUser = async (req, res) => {
       await sendWelcomeOrgEmail(email, userData.name.first)
     }
 
-    
-    
+
+
     // Respond with success message, status, and user data
     res.status(200).json({
       msg: "User registered successfully",
@@ -425,7 +425,7 @@ export const login = async (req, res) => {
     if (email) {
       query = { "email.id": email.toLowerCase().trim() };
     } else {
-      query = { "phone.number": mobileNo };  
+      query = { "phone.number": mobileNo };
     }
 
     const user = await User.findOne(query);
